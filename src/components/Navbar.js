7dba3449c8ae582c020d58014557af40f21fba2a
Barayuda Gautama <barayuda.gautama@dana.id>
2022-02-10 00:47:08 +0800

    fix: navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,6 +44,7 @@ const toggleButton = css({
 	marginLeft: '2rem',
 	fontSize: '2rem',
 	background: 'none',
+  cursor: 'pointer',
 })
 
 const navbarMenuWrapper = css({
@@ -86,16 +87,12 @@ function Navbar() {
 		<>
 			<IconContext.Provider value={{color: '#fff'}}>
 				<div css={navbar}>
-					<Link to="#" css={toggleButton}>
-						<FaIcons.FaBars onClick={showSideBar} />
-					</Link>
+          <FaIcons.FaBars css={toggleButton} onClick={showSideBar} />
 				</div>
 				<nav css={sideBar({sidebar})}>
 					<ul css={navbarMenuWrapper} onClick={showSideBar}>
 						<li css={toggleButtonWrapper}>
-							<Link to='#' css={toggleButton}>
-								<IoIcons.IoCloseOutline />
-							</Link>
+              <IoIcons.IoCloseOutline css={toggleButton} />
 						</li>
 						{SideBar.map((item, index) => {
 							return (
